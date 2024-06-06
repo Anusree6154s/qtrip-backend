19fcf4abb0cd31fc6d023353283d72e07f495688
Anusree Anilkumar <anilkumaranusree113@gmail.com>
2024-06-06 16:53:30 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "http://3.110.80.110:8082" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -3,7 +3,7 @@ import config from "../conf/index.js";
 async function init() {
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
-
+  console.log(cities);
   //Updates the DOM with the cities
   if (cities) {
     cities.forEach((key) => {
@@ -16,14 +16,23 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    let response = await fetch(config.backendEndpoint + "/cities");
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-
+  let div = document.getElementById("data");
+  let card = `<a class="tile" id="${id}" href="pages/adventures/?city=${id}"> <img src='${image}' ></img> <div class='tile-text' > <p>${city}</p> <p>${description}</p> </div></a>`;
+  div.innerHTML = div.innerHTML + card;
+  console.log(div);
 }
 
 export { init, fetchCities, addCityToDOM };
